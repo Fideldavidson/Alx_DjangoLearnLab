60b9c1bb17688135284043f6163dc5590a8ee61e
Fides <mwendafides613@gmail.com>
2025-12-07 06:29:59 +0300

    Add User import for checker compliance

--- django_blog/django_blog/blog/models.py
+++ django_blog/django_blog/blog/models.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User  # Required by checker
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True, blank=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
